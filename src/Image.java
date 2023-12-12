3cf652aa83b107c1204d361a600f124a154c4589
patdziedzic <danieldziedzic93@gmail.com>
2023-12-12 00:37:46 -0500

    Started setting up Task 2

--- src/Image.java
+++ src/Image.java
@@ -1,5 +1,5 @@
 public class Image {
     PixelVector[] image = new PixelVector[Main.D*Main.D + 1];
-    //PixelVector[] weightVector = new PixelVector[Main.D*Main.D + 1];
     boolean dangerous;
+    String colorToCut;
 }
